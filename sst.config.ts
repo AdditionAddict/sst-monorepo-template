5c6ff0ed38bfd0443798c6dff08bd8d97acc72ab
Andrew Allen <andrew@Andrews-MacBook-Air.local>
2024-09-04 02:32:24 +0100

    rename

--- sst.config.ts
+++ sst.config.ts
@@ -3,7 +3,7 @@
 export default $config({
   app(input) {
     return {
-      name: "monorepo-template",
+      name: "adhd-lifestyle-pro",
       removal: input?.stage === "production" ? "retain" : "remove",
       home: "aws",
     };
